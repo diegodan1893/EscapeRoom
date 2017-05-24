79f498fc39c603a314ebb70c72bad0420bff7b16
Gerardo López <kirbuster@hotmail.com>
2017-05-24 17:02:00 +0200

    ObjetoRecogible Done

---
+++ js/ObjetoRecogible.js
@@ -0,0 +1,38 @@
+/** Clase objeto recogible */
+
+/**
+ * Objeto Recogible
+ * 
+ * @param {Object3D} modelo Modelo inicial.
+ * @param {FuncionInteraccion} funcionInteraccion Función utilizada para la interacción.
+ * @param {ObjetoInventario} objetoActivacion Objeto utilizado para interactuar con el entorno.
+ * @param {ObjetoInventario} objetoRecoger Objeto que sustituye este por el del inventario.
+ * @param {Juego} juego Propio juego.
+ */
+
+
+ObjetoRecogible = function(modelo, funcionInteraccion, objetoActivacion, objetoRecoger, juego)
+{
+    ObjetoInteractuable.call(this,modelo,funcionInteraccion, objetoActivacion);
+
+    this.juego = juego;
+    this.objetoARecoger = objetoRecoger;
+
+};
+
+ObjetoRecogible.prototype = Object.create(ObjetoInteractuable.prototype);
+ObjetoRecogible.prototype.constructor = ObjetoRecogibles;
+
+ObjetoRecogible.prototype.interactuar = function(modo, objetoSeleccionado)
+{
+    var resultado = false;
+    if (objetoSeleccionado === null){
+        this.juego.DarObjeto(this.objetoARecoger);
+    }else if (objetoSeleccionado === this.objetoActivacion){
+        this.juego.DarObjeto(this.objetoARecoger);
+    }
+
+    resultado = super.interactuar(modo, objetoSeleccionado);
+
+    return resultado;
+}
